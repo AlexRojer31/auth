4081eb1e937beb6f3ae69ef08551092fb1c5da35
Alexandr Ronzhin <alexandrronzhin@yandex.ru>
2024-05-02 21:04:40 +0300

    upd

--- src/common/session/session.service.ts
+++ src/common/session/session.service.ts
@@ -1,6 +1,14 @@
 'use strict';
 
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Session } from './session.entity';
 
 @Injectable()
-export class SessionService {}
+export class SessionService {
+  constructor(
+    @InjectRepository(Session)
+    private readonly repo: Repository<Session>,
+  ) {}
+}
--- src/common/user/user.service.ts
+++ src/common/user/user.service.ts
@@ -1,6 +1,14 @@
 'use strict';
 
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repo: Repository<User>,
+  ) {}
+}
